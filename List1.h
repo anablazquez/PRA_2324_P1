fe60209653ad989ac746f22b68cc638bbdb19a4e
ABLASAN <ablasan@alumno.upv.es@ldsic-vdi06.upvnet.upv.es>
2023-10-24 14:34:26 +0200

    Añadida interfaz genérica List

---
+++ List1.h
@@ -0,0 +1,19 @@
+#ifndef LIST_H
+#define LIST_H
+
+template <typename T>
+class List {
+    public:
+            virtual void insert(int po, T e);
+             virtual void append(T e);
+             virtual void prepend(T e);
+             virtual T remove(int pos);
+             virtual T get(int pos);
+             virtual int search(T e);
+             virtual bool empty();
+              virtual int size();
+
+        // ... aquí los métodos virtuales puros
+};
+
+#endif
