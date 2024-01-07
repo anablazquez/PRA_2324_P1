e49d5a360ccd457b7a173c2094c12e816e3387ec
ABLASAN <ablasan@alumno.upv.es@ldsic-vdi18.upvnet.upv.es>
2024-01-07 14:48:22 +0100

    Añadida implementación de la clase ListLinked

--- ListArray.h
+++ ListArray.h
@@ -1,66 +1,87 @@
 #include <ostream>
 #include "List.h"
 
-template <typename T> 
-class ListArray : public List<T> {
+template <typename T>
 
-    private:
-      T *arr;
-      int max;
-       int n;
-        static const int MINSIZE=2;
-	void resize(int new_size){//redimensiona el array
-		T aux[new_size];
-		for(int i=0; i< new_size; i++){
-			arr[i]=aux[i];}//copia el contenido de un array a otro
-			delete arr;
-		arr= new int [new_size];//actualizar puntero para que apunte a la direccion de memoria 
-		max=new_size;}
-    public:
-	
-        ListArray(){
-		arr=new T [MINSIZE];
-		max=2;//inicializa
-		n=0;}//inicializa
-	~ListArray(){// libera memoria dinamica
-		delete[] arr;}	
-	T operator[](int pos){// sobrecarga del operador 
-		if(pos>n|| pos<0){//fuera del intervalo
-			throw std::out_of_range("Numero fuera del rango");}
-		else return arr[pos];}
-	friend std:: ostream& operator <<(std::ostream &out,const ListArray<T> &list){
-		out<<"List ->[ ";//imprimo el corchete
-		for(int i=0;i<list.n;i++){//recorro
-			out<<list.arr[i]<<" ";} 
-		out<<"]";
-		return out;}
-	
-	virtual void insert(int pos, T e ) override{ 
-	if(pos>n|| pos<0){
-		throw std::out_of_range("fuera del intervalo");}
+class ListArray : public List<T>{
 
-		if(size()==max){
-			resize(size()*2);}
-		else if(pos==0){
-			prepend(e);}
-		else if(pos==n){
-			append(e);}
-		else{ 
-			for (int i=n; i> pos ;i--){
-				arr[i]=arr[i-1];}
-			arr[pos]=e;
-			n++;}
-	}
-	virtual void  append(T e) override{
-		if(size() == max)
+ private:
+  // miembros privados
+  T* arr;
+  int max;
+  int n;
+  static const int MINSIZE = 2;
+  void resize(int new_size){
+   
+     T aux[new_size];
+      for(int i = 0; i < new_size ;i++)
+        aux[i] = arr[i];
+
+      delete arr;
+      
+      arr = new int [new_size];
+      for(int i = 0; i < new_size ;i++)
+        arr[i] = aux[i];
+
+      max = new_size;
+    
+  }
+
+ public:
+  
+  ListArray(){
+ 
+    arr = new int [MINSIZE];
+    max = 2;
+    n = 0;
+  }
+  
+  ~ListArray(){
+    delete[] arr;
+  }
+
+  T operator[](int pos){
+    if(pos > n || pos < 0){
+      throw std::out_of_range("Número fuera de rango del array");
+    }
+    else 
+      return arr[pos];
+  }
+  
+  friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
+    out<<"List -> [ ";
+	  for(int i = 0; i < list.n ; i++){
+      out<< list.arr[i]<<" ";
+
+    }
+	  out<<"]";
+    return out;
+  }
+
+  virtual void insert(int pos, T e) override{
+    if(pos > n || pos < 0){
+      throw std::out_of_range("Número fuera de rango del array");
+    }
+     if(size() == max)
              resize(size()*2);
 
-      arr[n] = e;
-      n++;
-    
-	}
-	virtual  void prepend (T e) override{
-		 if(size() == max)
+      if (pos == 0){
+	prepend(e);
+      }
+      else if(pos == n){
+	append(e);
+      }
+      else{
+	      for(int i = n ; i > pos ; i--){
+		      arr[i] = arr[i-1];	 		
+	      }
+	      arr[pos] = e;
+	      n++;
+      }
+ 	
+  } 
+  virtual  void prepend(T e) override{
+      if(size() == max)
 	     resize(size()*2);
       else{
 	      for(int i = n; i > 0; i --){
@@ -72,41 +93,59 @@ arr[0] = e;
       } 
     }
     
-	 virtual  T remove (int pos) override{
-		 T aux;
-		  if(pos > size()-1 || pos < 0){
-			throw std::out_of_range("Número fuera de rango del array");}
-      		else{
-	  		aux = arr[pos];  
-	    
-	   		 for(int i = pos; i < n; i++){
-				arr[i] = arr[i+1];
-	      	}
-	  		 n--;
-		return aux;
-      }
-	 }
-	 virtual T get (int pos)override{
-	 
-	  if(pos > size()-1 || pos < 0){
-		throw std::out_of_range("Número fuera de rango del array");
-      }
-    	 else{
-		return arr[pos]; }
-	 }
-	 virtual  int search(T e) override{
-	 	for( int i =0; i<n; i++){
-			 if(arr[i]==e)
-			return i;}
-	 	return -1;}
-	 
-	
-	virtual  bool empty() override{ 
-	if (n==0){ return true;}
-	 else{ return false;}}
-	 virtual  int size() override{return n ;}
-
-	// miembros públicos, incluidos los heredados de List<T>
     
-};
+    
+    
+  virtual void append(T e) override{
+      if(size() == max)
+             resize(size()*2);
 
+      arr[n] = e;
+      n++;
+    }
+
+  virtual   T remove(int pos) override{
+      T aux;
+	  if(pos > size()-1 || pos < 0){
+	throw std::out_of_range("Número fuera de rango del array");
+      }
+      else{
+	    aux = arr[pos];  
+	    
+	    for(int i = pos; i < n; i++){
+		arr[i] = arr[i+1];
+	      }
+	   n--;
+	return aux;
+      }
+      
+    }
+    
+  virtual T get(int pos) override{
+      if(pos > size()-1 || pos < 0){
+	throw std::out_of_range("Número fuera de rango del array");
+      }
+      else{
+	return arr[pos];
+      }
+    }
+  virtual int search(T e) override{
+      for(int i = 0; i < n ; i++){
+	if ( arr[i] == e)
+	  return i;
+      }
+      return -1;
+    }
+  virtual bool empty() override{
+      if(n == 0)
+	return true;
+      else return false;
+    }
+    
+  virtual int size() override{
+      return n;
+    }
+   
+    // miembros públicos, incluidos los heredados de List<T>
+  };
+	
--- ListLinked.h
+++ ListLinked.h
@@ -3,51 +3,159 @@
 #include "Node.h"
 
 template <typename T>
-class ListLinked : public List<T> {
 
-    private:
- 
-        Node<T>* first;
-	int n ;
-
-
-    public:
-
-        ListLinked(){
-		n=0;
-		first= nullptr;
-	}
-	~ListLinked(){
-		 Node<T>* toDelete = nullptr; 
-		while(first!= nullptr){
-		 Node<T>* aux = first->next;
-		toDelete = first; // copiamos dirección de memoria
-		delete toDelete; // liberamos memoria
-		first =aux;}}
-
-
-
-	T operator[](int pos){
-		if( pos> n|| pos<0){
-			throw std::out_of_range ("posicion no valida");}
-		else{
-			 Node<T>* aux= first;
-			  for(int i=1; i<pos;i++){ //recorremos hasta que encontremos la posicion deseada
-				  aux=aux-> next;}//pasamos al nodo iguiente 
-			  return aux->data;}}
-				
-		friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list){
-				out<<"List -> [ ";
+class ListLinked : public List<T>{
+  
+ private:
+  // miembros privados
+  Node<T>* first;
+  int n;
+  
+public:
+  
+  ListLinked(){
+    n = 0;
+    first = nullptr;
+  }
+  
+  ~ListLinked(){
+	  while(first != nullptr){
+	    Node<T>* aux = first->next;
+	    delete first;
+	    first = aux;
+	  }
+  }
+  
+  T operator[](int pos){
+    if(pos > n || pos < 0){
+      throw std::out_of_range("Número fuera de rango del array");
+    }
+    else{
+      Node<T>* aux = first;
+      for(int i = 1; i < pos; i ++){
+        aux = aux ->next;
+        }
+      return aux->data;
+    }
+  }
+  
+  friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list){
+    out<<"List -> [ ";
     
-   				 Node<T>* aux = list.first;
+    Node<T>* aux = list.first;
     
-   			 	while(aux){
-      					out<< aux->data;
-      					if(aux->next != nullptr)// hasta que el contenico no sea 0 seguir
-						out<<",";
+    while(aux){
+      out<< aux->data;
+      if(aux->next != nullptr)
+	out<<",";
       
-     					 aux = aux->next;
-    							}
-   				 out<<"]";
-      				 return out;} 
-	};
+      aux = aux->next;
+    }
+    out<<"]";
+    return out;
+  }
+  virtual  void prepend(T e) override{
+    Node <T> *aux = new Node<T>(e, first);
+    first = aux;
+    n++;
+    }
+  
+  
+  virtual void append(T e) override{
+    if (first == nullptr)
+      prepend(e);
+    else{
+      
+      Node<T> * act = first;
+      for(int i = 1; i< size(); i++){	 
+	act = act->next;	
+      }
+      Node<T>* aux = new Node<T>(e , act->next);
+      act->next = aux;
+      
+    }
+    n++;
+  }
+  
+  virtual void insert(int pos, T e) override{
+    if(pos > n || pos < 0){
+      throw std::out_of_range("Número fuera de rango del array");
+    }
+    if(pos == 0)
+      prepend(e);
+    
+    else if(pos == n)
+      append(e);
+    
+    else{
+      
+      Node<T> * act = first;
+      for(int i = 1; i < pos ; i++){
+	act = act->next;
+      }
+      Node<T>* aux = new Node<T>(e, act->next);
+      act ->next =aux ;
+      n++;
+    }
+  }
+  
+  
+  T remove(int pos) override{
+            if (pos < 0 || pos >= n) { throw out_of_range("Posición no válida del array\n"); }
+            else{
+                T x;
+                if(pos == 0){
+                    Node<T>* aux = first;
+                    x = aux->data;
+                    first = first->next;
+                    delete aux;
+                }else{
+                    Node<T>* aux = first->next;
+                    Node<T>* prev = first;
+                    for(int i = 1; i < pos; i++){
+                        aux = aux->next;
+                        prev = prev->next;
+                    } 
+                    x = aux->data;
+                    prev->next = aux->next;
+                    delete aux;
+                } 
+                n--;
+                return x;
+            } 
+        } 
+
+  virtual T get(int pos) override{
+    if(pos > size()-1 || pos < 0){
+      throw std::out_of_range("Número fuera de rango del array");
+    }
+    else{
+      Node<T>* aux = first;
+      for(int i = 0; i < pos; i ++){
+	aux = aux ->next;
+	}
+      return aux->data;
+    }
+  }
+  virtual int search(T e) override{
+    Node<T>* aux = first;
+    for(int i = 0; i < n ; i++){
+      
+      if ( aux ->data == e){
+	return i;
+      }
+      aux= aux->next;
+    }
+    return -1;
+  }
+  virtual bool empty() override{
+    if(n == 0)
+	return true;
+    else return false;
+  }
+  
+  virtual int size() override{
+    return n;
+  }
+  
+};
